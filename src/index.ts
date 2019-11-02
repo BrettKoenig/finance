78d8ac39157456199953eee4b1b9d1dfbccdb1d1
Brett Koenig <koenig.337@osu.edu>
2019-11-02 11:36:56 -0400

    removing erros

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,17 @@
 import { GoalCalculator } from './services/goalCalculator'
 import { CsvDataRetriever } from './services/csvDataRetriever'
-import { PriorityConsideration } from './models/priorityConsideration';
-import moment from 'moment';
-import { ExpenseAggregate, Expense } from './models';
+import { ExpenseAggregate, Expense, PriorityConsideration } from './models'
+import moment from 'moment'
 
 const goalCalculator = new GoalCalculator();
 const dataRetriever = new CsvDataRetriever();
 
 async function calculateGoals() {
-  const goalAggregate = await dataRetriever.getGoalAggregate();
-  const accountAggregate = await dataRetriever.getAccountAggregate();
-  const budgetAggregate = await dataRetriever.getBudgetAggregate();
-  const expenses = await dataRetriever.getExpenses()
-
+  const goalAggregate = await dataRetriever.getGoalAggregate()
+  const accountAggregate = await dataRetriever.getAccountAggregate()
+  const budgetAggregate = await dataRetriever.getBudgetAggregate()
   const expenseAggregate = await dataRetriever.getExpenseAggregate()
 
-  expenseAggregate.findExpensesForMonth(moment("01/01/2017", "MM/DD/YYYY").toDate())
-
-  const januaryExpenses = expenseAggregate.findExpenseByDateRange(moment("01/01/2018"), moment("01/09/2018"))
-  januaryExpenses.forEach((expense: Expense) => {
-    console.log(expense.Date)
-    console.log(expense.Description)
-    console.log(expense.Amount)
-    console.log(expense.Category)
-    console.log('---------')
-  })
   // goalCalculator.calculateByDate(goalAggregate, accountAggregate)
   // goalCalculator.calculateByPriority(goalAggregate, accountAggregate)
   // goalCalculator.calculateWeightedWithPriority(goalAggregate, accountAggregate, PriorityConsideration.None);
