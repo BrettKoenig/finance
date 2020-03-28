30e9c9ff1c8c726c58ddafa22f995ccc6d18a2bf
Brett Koenig <koenig.337@osu.edu>
2020-03-27 21:15:47 -0400

    dashboard work

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from 'react'
 import Data from './data'
-import './App.css';
+import './App.css'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <Data name={'expense'} />
-      <Data name={'account'} />
-      <Data name={'budget'} />
-      <Data name={'goal'} />
-    </div>
-  );
+  return (<Tabs>
+      <TabList>
+        <Tab>Expense</Tab>
+        <Tab>Accounts</Tab>
+        <Tab>Budgets</Tab>
+        <Tab>Goals</Tab>
+      </TabList>
+
+      <TabPanel>
+        <Data name={'expense'} />
+      </TabPanel>
+      <TabPanel>
+        <Data name={'account'} />
+      </TabPanel>
+      <TabPanel>
+        <Data name={'budget'} />
+      </TabPanel>
+      <TabPanel>
+        <Data name={'goal'} />
+      </TabPanel>
+    </Tabs>
+  )
 }
 
-export default App;
+export default App
